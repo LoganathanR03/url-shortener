09ea6befb48abf3ceb9384d2abbb9063597b653a
Loganathan R <logangreen1210@gmail.com>
2025-07-25 22:32:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,4 @@ def redirect_url(short_code):
         return 'Invalid or expired short URL.', 404
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
